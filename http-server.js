80299495d2d41081be992007afbdcd97f13b8c64
Gary J <github@accu.ca>
2021-11-23 11:19:26 -0800

    Add paths

--- http-server.js
+++ http-server.js
@@ -2,9 +2,24 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
   const method = request.method;
-  const url = request.url;
+  const path = request.url;
 
-  console.log("Someone sent a request:", method, url);
+  console.log("Someone sent a request:", method, path);
+
+  if (method === "GET" && path === "/info") {
+    response.write("This is the Info page");
+    response.end();
+    return;
+  }
+
+  if (method === "GET" && path === "/home") {
+    response.write("This is the Home page");
+    response.end();
+    return;
+  }
+
+  response.write("Page not Found:". );
+  response.end();
 });
 
 server.listen(8000, () => {
