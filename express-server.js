28d2181628f3a1014b61aaded91bd0c092aa982b
Gary J <github@accu.ca>
2021-11-23 14:36:15 -0800

    add Morgan

--- express-server.js
+++ express-server.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 const port = 1234;
 
 // middleware
-app.use((req, res, next) => {
-  console.log('incoming request:', req.method, req.url);
-  // res.send('hello from the middleware');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('incoming request:', req.method, req.url);
+//   // res.send('hello from the middleware');
+//   next();
+// });
+
+app.use(morgan('dev'));
 
 app.get("/info", (req, res) => {
   res.send("This is the Info page");
