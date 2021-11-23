a65ec444a73f583f1fa7336a670fae1c0a443d77
Gary J <github@accu.ca>
2021-11-23 14:18:44 -0800

    add default

--- express-server.js
+++ express-server.js
@@ -4,11 +4,20 @@ const app = express();
 const port = 1234;
 
 app.get("/info", (req, res) => {
-  res.send("This is the Info page")
+  res.send("This is the Info page");
 });
 
 app.get("/home", (req, res) => {
-  res.send("This is the Home page")
+  res.send("This is the Home page");
+});
+
+app.get('*', (req, res) => {
+  // res.status(404);
+  res.statusCode = 404;
+  res.send('Page not Found:');
+  return;
+
+  // res.status(404).send('Page not Found');
 });
 
 app.listen(port, () => {
