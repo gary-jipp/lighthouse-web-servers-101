e92a5cd4080309688e3297f9a627660b6b396f05
Gary J <github@accu.ca>
2021-11-23 12:44:26 -0800

    add endpoints

--- express-server.js
+++ express-server.js
@@ -3,6 +3,13 @@ const express = require('express');
 const app = express();
 const port = 1234;
 
+app.get("/info", (req, res) => {
+  res.send("This is the Info page")
+});
+
+app.get("/home", (req, res) => {
+  res.send("This is the Home page")
+});
 
 app.listen(port, () => {
   console.log(`the app is listening on port ${port}`);
